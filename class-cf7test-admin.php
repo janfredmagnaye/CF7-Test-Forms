d99ef989815b1d1bb953703b402c4aa6351f1789
janfredmagnaye <52711986+janfredmagnaye@users.noreply.github.com>
2021-04-23 13:04:12 -0700

    Added new fields for new features

--- class-cf7test-admin.php
+++ class-cf7test-admin.php
@@ -14,11 +14,26 @@ function cf7test_render_plugin_settings_page() {
         settings_fields( 'cf7test_plugin_options' );
         do_settings_sections( 'cf7test_plugin' );
         ?>
-        <table>
+        <table class="cf7-test-table">
+            <tr class="cf7-test-section">
+                <td><h3>Admin-only Settings</h3></td>
+            </tr>
             <tr>
-                <td><strong>Enable CF7 Test Mode</strong></td>
+                <td><strong>Disable Sending Email</strong></td>
                 <td><input type="checkbox" name="cf7-test-mode" value="true" <?php if(get_option('cf7-test-mode') == "true") echo "checked"; ?> /></td>
             </tr>
+            <tr>
+                <td><strong>Custom Email Receiver <br>( Uncheck the option above to use this )</strong></td>
+                <td><input type="email" name="cf7-email-receiver" value="<?= esc_attr( get_option('cf7-email-receiver') ) ?>" /></td>
+            </tr>
+            <tr class="cf7-test-section">
+                <td><h3>CF7 Form Add-ons</h3></td>
+            </tr>
+            <tr>
+                <td><strong>Enable Spam Filtering</strong></td>
+                <td><input type="checkbox" name="cf7-spam-filter" value="true" <?php if(get_option('cf7-spam-filter') == "true") echo "checked"; ?> /></td>
+            </tr>
+
         </table>
         <?= submit_button(); ?>
     </form>
@@ -27,6 +42,8 @@ function cf7test_render_plugin_settings_page() {
 
 function cf7test_register_settings() {
     register_setting( 'cf7test_plugin_options', 'cf7-test-mode');
+    register_setting( 'cf7test_plugin_options', 'cf7-spam-filter');
+    register_setting( 'cf7test_plugin_options', 'cf7-email-receiver');
 }
 
 add_action( 'admin_init', 'cf7test_register_settings' );
