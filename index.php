ddc180b54164dfd263065ef4c8725871b7810dc0
janfredmagnaye <52711986+janfredmagnaye@users.noreply.github.com>
2021-04-23 12:44:06 -0700

    Added more features
    + CSS on Settings Page
    + Custom Email Receiver when logged-in as Admin
    + Added Text-area Spam Filtering

--- index.php
+++ index.php
@@ -33,8 +33,29 @@ function registerToggleButton($wp_admin_bar){
     );
     $wp_admin_bar->add_node($args);
 }
+
 add_action('admin_bar_menu', 'registerToggleButton', 999);
 
-// Register the Option
+// Load Styles for Admin
+add_action( 'admin_enqueue_scripts', 'cf7_test_admin_styles' );
+function cf7_test_admin_styles(){
+    wp_enqueue_style( 'cf7_test_admin_css', plugin_dir_url( __FILE__ ) .  '/assets/css/cf7-test-mode.css', false, '1.0.0' );
+}
 
+// Filter Text areas
+add_filter('wpcf7_validate_textarea', 'cf7_test_textarea_filter', 10, 2);
+function cf7_test_textarea_filter($result, $tag) {  
+    if(get_option('cf7-spam-filter') == "true"){        
+        $type = $tag['type'];
+        $name = $tag['name'];
+        $value = $_POST[$name];
+        if(preg_match('/\b(?:(?:https?|ftp):\/\/)?[\w\-?=%.]+\.[\w\-&?=%.]+/i', $value) ){
+            $result->invalidate( $tag, "Please remove URLs, links, or emails on this field to continue." );
+        }
+        if(preg_match('/[А-Яа-яЁё]/u', $value )){
+            $result->invalidate( $tag, "Please remove any Cyrillic alphabet." );
+        }
+        return $result;
+    }
+}
 ?>
